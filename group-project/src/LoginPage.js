790cc4b1a13bb4ebc1a2e668f176c55877ebb65d
ktran44 <98405222+ktran44@users.noreply.github.com>
2023-02-23 19:02:06 -0600

    Add files via upload

---
+++ group-project/src/LoginPage.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import './LoginPage'; 
+
+function LoginPage(){
+    const [display,setDisplay]=useState(false)
+    const [show,setShow]=useState(false)
+    const handleShow=()=>
+    {
+      setShow(!show)
+    }
+return(
+    <div>
+        <div className='container'>
+            <div> 
+                {
+                display?<div>
+                    <h1 className='title'>Sign Up</h1>
+                    <div>
+                        <div className='field_container'>
+                        <input placeholder='First Name' className='name'/>
+                        <input placeholder='Last Name' className='name'/>
+                        </div>
+                        <div className='field_container'>
+                            <input placeholder='Email' className='otherField'/>
+                        </div>
+                        <div className='field_container'>
+                        <input placeholder='Password' type={show?"text":"password"} className='otherField'/>
+                        {/* <label onClick={handleShow}>{show?"Hide":"Show"}</label> */}
+                        </div>
+                        <button className='login'>Register</button>
+                    </div>
+                </div>:
+                <div>
+                <h1 className='title'>Login</h1>
+                <div>
+                    <div className='field_container'>
+                        <input placeholder='Email' className='otherField'/>
+                    </div>
+                    <div className='field_container'>
+                    <input placeholder='Password' type={show?"text":"password"} className='otherField'/>
+                    {/* <label onClick={handleShow}>{show?"Hide":"Show"}</label> */}
+                    </div>
+                    <button className='login'>Login</button>
+                </div>
+            </div>
+                }
+                <div className={!display?'login_button':'main_button'}>
+                    <button className='sign' onClick={()=>setDisplay(true)}>Register</button>
+                    <button className='sign' onClick={()=>setDisplay(false)}>Login</button>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+}
+export default LoginPage;
